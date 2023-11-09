0287b0bb8cec5e60ea8cfecd83a56aa9a6f4ea7c
eduardo carmo <ext.eduardo.junior@burgerking.com.br>
2023-11-09 05:34:36 -0300

    Ajuste security

--- modules/aws/security_group.tf
+++ modules/aws/security_group.tf
@@ -7,25 +7,16 @@ resource "aws_security_group_rule" "ssh_cluster_in" {
   type              = "ingress"
   from_port         = 0
   to_port           = 0
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"] #0.0.0.0 - 255.255.255.255
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.ssh_cluster.id
 }
 
-resource "aws_security_group_rule" "eks_sg_ingress_rule" {
-    cidr_blocks = ["0.0.0.0/0"]
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-
-    security_group_id = aws_security_group.ssh_cluster.id
-    type = "ingress"
-} 
 resource "aws_security_group_rule" "ssh_cluster_out" {
   type              = "egress"
   from_port         = 0
   to_port           = 0
   protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"] #0.0.0.0 - 255.255.255.255
+  cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.ssh_cluster.id
 }
